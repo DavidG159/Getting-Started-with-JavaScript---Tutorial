3b8ddcdc6ca274988e4a70823d79fc8a388dd8bf
DavidG159 <gaspardavid235@gmail.com>
2024-06-03 15:29:30 -0600

    My First JS

---
+++ tny_script.js
@@ -0,0 +1,23 @@
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Sirxavier David C. Gaspar
+   Date: 2024-06-03
+
+*/
+
+//Display the current date & time
+document.getElementById("dateNow").innerHTML = "m/d/y<br />h:m:s";
+
+//Display the time left until New Year's End
+
+document.getElementById("days").innerHTML = "dd";
+
+document.getElementById("hrs").innerHTML = "hr";
+
+document.getElementById("mins").innerHTML = "mm";
+
+document.getElementById("secs").innerHTML = "ss";
