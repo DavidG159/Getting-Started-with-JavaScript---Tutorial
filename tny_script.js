db9850fb55a185b385f29538039dcebfbba7cc3b
DavidG159 <gaspardavid235@gmail.com>
2024-06-04 15:29:01 -0600

    My Commit JS2

--- tny_script.js
+++ tny_script.js
@@ -21,3 +21,39 @@ document.getElementById("hrs").innerHTML = "hr";
 document.getElementById("mins").innerHTML = "mm";
 
 document.getElementById("secs").innerHTML = "ss";
+
+//New variable for date-time
+var currentDay = new Date("May 23, 2018 14:35:05");
+var dateString = currentDay.toLocaleDateString();
+var timeString = currentDay.toLocaleTimeString();
+
+//We are dynamically setting Current time
+document.getElementById("dateNow").innerHTML = dateString + "<br />" + timeString;
+
+var newYear = new Date("January 1, 2018");
+
+var nextYear = currentDay.getFullYear() + 1;
+
+newYear.setFullYear(nextYear);
+
+var daysLeft = (newYear - currentDay)/(1000*60*60*24);
+
+//Display the time left until New Year's Eve
+days.innerHTML = daysLeft;
+
+days.textContent = Math.floor(daysLeft);
+
+//hours
+var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
+
+document.getElementById("hrs").textContent = Math.floor(hrsLeft);
+
+//minutes
+var minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
+document.getElementById("mins").textContent = Math.floor(minsLeft);
+
+//seconds
+var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
+document.getElementById("secs").textContent = Math.floor(secsLeft);
+
+console.log(daysLeft);
